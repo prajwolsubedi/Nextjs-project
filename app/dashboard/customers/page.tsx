d9ede4d31d47d754ff431a7fc1e8325761e71e3f
prajwol subedi <prajwolsubedizzz@gmail.com>
2026-01-27 14:51:05 +0545

    update: customer page dashboard updated

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+"use client";
+export default async function Page() {
+  // console.log("Fetching revenue data...");
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <p>Customers Page</p>;
 }
